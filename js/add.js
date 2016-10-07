e79d8be36c7bd82fcc3e8e3c71f12bfe668dedce
Tommy Waters <Thomas.Waters05@gmail.com>
2016-10-06 21:16:00 -0500

    adding MH3 bare bones files, html, etc

---
+++ js/add.js
@@ -0,0 +1,13 @@
+
+// **HIDES/ADDS - THIS IS STANDARD FROM INSTRUCTIONS OF MH3**
+var addLink = document.getElementById("link-add");
+var addView = document.getElementById("add-view");
+
+addLink.addEventListener("click", function() {
+  homeView.classList.add("hidden");
+  listView.classList.add("hidden");
+
+  addView.classList.add("visible");
+  addView.classList.remove("hidden");
+
+});
